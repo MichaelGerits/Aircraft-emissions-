c40b95de59e025af6ca9f59ea3f97aad72530063
MatyasGerlei <matyas.gerlei.uni@gmail.com>
2025-02-20 15:09:21 +0100

    bababoey 2

--- Code/Package_test.py
+++ Code/Package_test.py
@@ -1,8 +1,10 @@
 import pandas # data loading in, reads csv files quickly and easily
 
-import OpenAP
+#import OpenAP
 #import fastmeteo
 import matplotlib
 #import seaborn
 #import cartopy
 
+df = pandas.read_csv("Data/Route_2003.csv")
+print(df.head())
