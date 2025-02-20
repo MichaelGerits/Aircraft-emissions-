0ca775a0b3687c893c5209d34b82ae9ca572b233
Your Name <carl.vistisen@gmail.com>
2025-02-20 16:26:02 +0100

    new

--- Code/Distancecal.py
+++ Code/Distancecal.py
@@ -2,11 +2,6 @@ import haversine as hs
 from haversine import Unit
 #using this method it is possible to calculate the distance between two point using (longitude and lattitude)
 
-
-lyon = (45.7597, 4.8422) # (lat, lon)
-paris = (48.8567, 2.3508)
-
-
 lyon = (45.7597, 4.8422) # (lat, lon)
 paris = (48.8567, 2.3508)
 
