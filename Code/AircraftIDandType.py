61fb689a08ec8719607734134f2ef40ffc2e48ae
Your Name <carl.vistisen@gmail.com>
2025-02-25 11:26:52 +0100

    change

--- Code/AircraftIDandType.py
+++ Code/AircraftIDandType.py
@@ -11,14 +11,38 @@ import pandas as pd # data loading in, reads csv files quickly and easily
 import matplotlib as mpl
 import openap
 from csv import DictReader
+from openap import (
+    Drag,
+    Thrust,
+    WRAP,
+    FlightPhase,
+    FlightGenerator,
+    FuelFlow,
+    Emission,
+)
+from openap import prop
+from pprint import pprint
 AircraftID = pd.read_csv("Data\Aircraft ID.csv", usecols=['ECTRL ID','AC Type'])
 ECTRL = pd.read_csv("Data\Aircraft ID.csv", usecols=['ECTRL ID'])
 ACType = pd.read_csv("Data\Aircraft ID.csv", usecols=['AC Type'])
-AircraftID_cleaned = AircraftID.drop_duplicates()
-AircraftID_cleaned.to_csv("Data\Aircraft_ID_Cleaned.csv", index=False)
+AircraftID['AC Type'] = AircraftID['AC Type'].str.lower()
+
+#AircraftID_cleaned = AircraftID.drop_duplicates()
+#AircraftID_cleaned.to_csv("Data\Aircraft_ID_Cleaned.csv", index=False)
+
+#AircraftTypes = ACType.drop_duplicates()
+#AircraftTypes.to_csv("Data\Aircraft_types.csv", index=False)
 
 df = pd.read_csv("Data\Aircraft_ID_Cleaned.csv")
-AircraftDictionary = df.set_index(['ECTRL ID'])['AC Type'].to_dict()
+AircraftDictionary = df.set_index(['ECTRL ID'])[('AC Type')].to_dict()
+
+avaiable_aircraft = prop.available_aircraft()
+
+print(f"Supports {len(avaiable_aircraft)} aircraft types")
+print(avaiable_aircraft)
 
 
-print(AircraftDictionary)
+
+
+
+
