abcbe2024847890d246c5d1438fbde106f82def5
Your Name <carl.vistisen@gmail.com>
2025-02-27 14:25:04 +0100

    d

--- Code/preprocessing/AircraftIDandType.py
+++ Code/preprocessing/AircraftIDandType.py
@@ -1,11 +1,3 @@
-#import haversine as hs   
-#from haversine import Unit
-#using this method it is possible to calculate the distance between two point using (longitude and lattitude)
-
-#lyon = (45.7597, 4.8422) # (lat, lon)
-#paris = (48.8567, 2.3508)
-
-#print(hs.haversine(lyon, paris))
 
 import pandas as pd # data loading in, reads csv files quickly and easily
 import matplotlib as mpl
