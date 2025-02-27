20ee8d64782749e9335a43c45bba2ce76b91e4cb
Your Name <carl.vistisen@gmail.com>
2025-02-27 15:54:45 +0100

    d

--- Code/preprocessing/Emmisions calculater.py
+++ Code/preprocessing/Emmisions calculater.py
@@ -1,8 +1,20 @@
 from AircraftIDandType import AircraftDictionary_Eurocontrol_and_Aircraft 
 import pandas as pd
 from preProcess import extract_ECTRLIDSeq 
+from openap import FuelFlow, Emission
 
+fuelflow = FuelFlow(ac="A320")
+emission = Emission(ac="A320")
 
+TAS = 350
+ALT = 30000
 
-print(extract_ECTRLIDSeq("Data\positions")[-1][238925257])
+FF = fuelflow.enroute(mass=60000, tas=TAS, alt=ALT, vs=0)  # kg/s
+
+CO2 = emission.co2(FF)  # g/s
+H2O = emission.h2o(FF)  # g/s
+NOx = emission.nox(FF, tas=TAS, alt=ALT)  # g/s
+CO = emission.co(FF, tas=TAS, alt=ALT)  # g/s
+HC = emission.hc(FF, tas=TAS, alt=ALT)  # g/s
+
 
