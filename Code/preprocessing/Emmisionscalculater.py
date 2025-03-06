b54d33a305b538415f6eac4eb5ea6968f51c668f
Your Name <carl.vistisen@gmail.com>
2025-03-06 17:02:49 +0100

    added comments

--- Code/preprocessing/Emmisionscalculater.py
+++ Code/preprocessing/Emmisionscalculater.py
@@ -12,23 +12,12 @@ import tracemalloc
 import matplotlib.pyplot as plt
 from AircraftIDandType import AircraftDictionary_Eurocontrol_and_Aircraft
 
-EuroControlID=238927422
-PlaneType=AircraftDictionary_Eurocontrol_and_Aircraft[EuroControlID]
-print(PlaneType)
+EuroControlID=238927688
+PlaneType=AircraftDictionary_Eurocontrol_and_Aircraft[EuroControlID] #defines the planetype that will be used 
+print(PlaneType)   
 m_A320=340000     #in kg 
 fuelflow = FuelFlow(ac=PlaneType)  #imports the specific flow for the type of aircraft
 emission = Emission(ac=PlaneType)  #imports the specific emmisions for the type of aircraft
-
-TAS = 350
-ALT = 30000
-
-FF = fuelflow.enroute(mass=m_A320, tas=TAS, alt=ALT, vs=0)  # kg/s
-
-CO2 = emission.co2(FF)  # g/s
-H2O = emission.h2o(FF)  # g/s
-NOx = emission.nox(FF, tas=TAS, alt=ALT)  # g/s
-CO = emission.co(FF, tas=TAS, alt=ALT)  # g/s
-HC = emission.hc(FF, tas=TAS, alt=ALT)  # g/s
 
 
 df=extract_ECTRLIDSeq('Data/PositionData/March')[EuroControlID]  #imports the data
@@ -44,18 +33,17 @@ Long=[]
 FL=[]
 
 
-for row in Time: 
+for row in Time: #imports the time as a list to make a list of hours and minutes 
     hours.append(int(row[-5:-3]))
     minutes.append(int(row[-2:]))
-Lat.extend(Lattitude.astype(float).tolist())  #makes a float list of lat
-Long.extend(Longitude.astype(float).tolist()), #makes a float list of long
-FL.extend(FlightLevel.astype(float).tolist()), # 
-print(Time)
-print(hours)
-delta=0
+Lat.extend(Lattitude.astype(float).tolist())  #makes a float list of lattitude 
+Long.extend(Longitude.astype(float).tolist()), #makes a float list of longtude 
+FL.extend(FlightLevel.astype(float).tolist()), #makes a float list of the flight level  
+
+delta=0    
 TimeStamp=[]
 Ttot=0
-def DeltaTime(h1,m1,h2,m2):
+def DeltaTime(h1,m1,h2,m2):   #makes a function that calculates the time difference between two time points (unit minuts)
     delta=0
     if m2>m1:
         delta=m2-m1
@@ -66,10 +54,10 @@ def DeltaTime(h1,m1,h2,m2):
     return delta
 
 Delta=[]
-for i in range(len(minutes)-2):
+for i in range(len(minutes)-2):    #makes a list of delta values.
     Delta.append(DeltaTime(hours[i],minutes[i],hours[i+1],minutes[i+1]))
 
-def DistanceCalc(lat1,long1,lat2,long2): #returns the distance in km 
+def DistanceCalc(lat1,long1,lat2,long2): #returns the distance betweem two points in km 
     p1=(lat1,long1)
     p2=(lat2,long2)
     D=haversine(p1,p2)
@@ -102,10 +90,10 @@ TotalCOEmmission=0
 
 
 for i in range(len(seq)-2):
-    DisTot=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])+DisTot
-    Dis=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])
-    Distances.append(DisTot)
-    DeltaTot=Delta[i]+DeltaTot
+    DisTot=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])+DisTot   #Caluculates the total distance travelled in km 
+    Dis=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])   #Calculate the distance between two sequence point in km 
+    Distances.append(DisTot)  # Makes a list of the total distances at a specific point in a journey 
+    DeltaTot=Delta[i]+DeltaTot  # creates 
     FLTot.append(FL[i])
     Ttot.append(DeltaTot)
     VerticalSpeed=VerSpeedCalc(Delta[i],FL[i],FL[i+1])
@@ -174,7 +162,37 @@ plt.ylabel("COEmmission (g/s)")
 plt.show()
 
 
+fig, axes = plt.subplots(2, 4, figsize=(15, 8))
 
+# First row
+axes[0, 0].scatter(Ttot, FLTot, marker=".", color='green', lw=0)
+axes[0, 0].set_ylabel("Altitude (ft)")
+
+axes[0, 1].scatter(Ttot, HorSpeeds, marker=".", color='blue', lw=0)
+axes[0, 1].set_ylabel("Speed (m/s)")
+
+axes[0, 2].scatter(Ttot, VerSpeeds, marker=".", color='red', lw=0)
+axes[0, 2].set_ylabel("ROC (fpm)")
+
+axes[0, 3].scatter(Ttot, FuelFlow1, marker=".", color='black', lw=0)
+axes[0, 3].set_ylabel("Fuel Flow (kg/s)")
+
+# Second row
+axes[1, 0].plot(Ttot, CO2Emmission, marker=".", color='green', linestyle='-', lw=2)
+axes[1, 0].set_ylabel("CO2 Emission (g/s)")
+
+axes[1, 1].plot(Ttot, H2OEmmission, marker=".", color='blue', linestyle='-', lw=2)
+axes[1, 1].set_ylabel("H2O Emission (g/s)")
+
+axes[1, 2].plot(Ttot, NOxEmmission, marker=".", color='red', linestyle='-', lw=2)
+axes[1, 2].set_ylabel("NOx Emission (g/s)")
+
+axes[1, 3].plot(Ttot, COEmmission, marker=".", color='black', linestyle='-', lw=2)
+axes[1, 3].set_ylabel("CO Emission (g/s)")
+
+# Adjust layout
+plt.tight_layout()
+plt.show()
 
 
 # Convert latitude and longitude to radians
