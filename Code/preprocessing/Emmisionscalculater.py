83c550408660a93f0f346e290e8d1a50ef1db925
Your Name <carl.vistisen@gmail.com>
2025-03-06 16:38:02 +0100

    fixed mistakes in CO2 calc

--- Code/preprocessing/Emmisionscalculater.py
+++ Code/preprocessing/Emmisionscalculater.py
@@ -12,10 +12,10 @@ import tracemalloc
 import matplotlib.pyplot as plt
 from AircraftIDandType import AircraftDictionary_Eurocontrol_and_Aircraft
 
-EuroControlID=238925267
+EuroControlID=238927422
 PlaneType=AircraftDictionary_Eurocontrol_and_Aircraft[EuroControlID]
 print(PlaneType)
-m_A320=181840     #in kg 
+m_A320=340000     #in kg 
 fuelflow = FuelFlow(ac=PlaneType)  #imports the specific flow for the type of aircraft
 emission = Emission(ac=PlaneType)  #imports the specific emmisions for the type of aircraft
 
@@ -69,18 +69,17 @@ Delta=[]
 for i in range(len(minutes)-2):
     Delta.append(DeltaTime(hours[i],minutes[i],hours[i+1],minutes[i+1]))
 
-def DistanceCalc(lat1,long1,lat2,long2):
+def DistanceCalc(lat1,long1,lat2,long2): #returns the distance in km 
     p1=(lat1,long1)
     p2=(lat2,long2)
     D=haversine(p1,p2)
     return D
 
 def VerSpeedCalc(dt,FL1,FL2):
-    v_vertical=(FL2-FL1)/dt*10/0.3048/60   #vertical flight speed in ft/min
+    v_vertical=(FL2-FL1)/dt*100   #vertical flight speed in ft/min
     return v_vertical
-def HorSpeedCalc(dt,D):      #horizontal flight speed in m/s
-    v_horizontal=(D)/dt/60*1000
-
+def HorSpeedCalc(dt,D):      #horizontal flight speed in kts
+    v_horizontal=(D)/dt/60*1000*1.943844
     return v_horizontal
 Distances=[]
 VerSpeeds=[]
@@ -91,6 +90,7 @@ DeltaTot=0
 Ttot=[]
 FLTot=[]
 FuelFlow1=[]
+TotalFuel=0
 CO2Emmission=[]
 H2OEmmission=[]
 NOxEmmission=[]
@@ -99,6 +99,8 @@ TotalCO2Emmission=0
 TotalH2OEmmission=0
 TotalNOxEmmission=0
 TotalCOEmmission=0
+
+
 for i in range(len(seq)-2):
     DisTot=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])+DisTot
     Dis=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])
@@ -110,14 +112,18 @@ for i in range(len(seq)-2):
     VerSpeeds.append(VerticalSpeed)
     HorizontalSpeed=HorSpeedCalc(Delta[i],Dis)
     HorSpeeds.append(HorizontalSpeed)
-    FF = fuelflow.enroute(mass=m_A320, tas=HorSpeeds[i], alt=FLTot[i], vs=VerSpeeds[i])
+    FF = fuelflow.enroute(mass=m_A320, tas=HorSpeeds[i], alt=FL[i]*100, vs=VerSpeeds[i])
+    TotalFuel=TotalFuel+FF*Delta[i]*60
     FuelFlow1.append(FF)
     CO2Emmission.append(emission.co2(FF))  # g/s
     H2OEmmission.append(emission.h2o(FF))  # g/s
-    NOxEmmission.append(emission.nox(FF, tas=HorSpeeds[i], alt=FLTot[i]))  # g/s
-    COEmmission.append(emission.co(FF, tas=HorSpeeds[i], alt=FLTot[i]))  # g/s
+    NOxEmmission.append(emission.nox(FF, tas=HorSpeeds[i], alt=FL[i]*100))  # g/s
+    COEmmission.append(emission.co(FF, tas=HorSpeeds[i], alt=FL[i]*100))  # g/s
+
+
+
 for i in range(len(CO2Emmission)-1):
-    TotalCO2Emmission=TotalCO2Emmission+abs(Delta[i]*(CO2Emmission[i+1]+CO2Emmission[i])/2)*60 # g
+    TotalCO2Emmission=TotalCO2Emmission+abs(Delta[i]*(CO2Emmission[i]))*60 # g
     TotalH2OEmmission=TotalH2OEmmission+abs(Delta[i]*(H2OEmmission[i+1]+H2OEmmission[i])/2)*60# g
     TotalNOxEmmission=TotalNOxEmmission+abs(Delta[i]*(NOxEmmission[i+1]+NOxEmmission[i])/2)*60# g
     TotalCOEmmission=TotalCOEmmission+abs(Delta[i]*(COEmmission[i+1]+COEmmission[i])/2) *60# g
@@ -126,6 +132,9 @@ print("Total CO2 emmisions [kg]=", TotalCO2Emmission/1000)
 print("Total H2O emmisions [kg]=", TotalH2OEmmission/1000)
 print("Total HOx emmisions [kg]=", TotalNOxEmmission/1000)
 print("Total CO emmisions [kg]=", TotalCOEmmission/1000)
+print("Total fuel spent=",TotalFuel)
+
+
 plt.subplot(221)
 plt.scatter(Ttot, FLTot, marker=".", color='green', lw=0)
 plt.ylabel("altitude (ft)")
