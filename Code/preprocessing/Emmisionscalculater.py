4ca2752542a13f6baafd5f05292d89673b614bd6
Michael Gerits <michael.gerits@alchemich.be>
2025-03-11 09:02:59 +0100

    sync

--- Code/preprocessing/Emmisionscalculater.py
+++ Code/preprocessing/Emmisionscalculater.py
@@ -5,8 +5,6 @@ from openap import FuelFlow, Emission
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 
 fuelflow = FuelFlow(ac="A320")
