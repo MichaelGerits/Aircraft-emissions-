2f3e26017934398df7c6cf4c0f3e908f58d557ef
Michael Gerits <michael.gerits@alchemich.be>
2025-03-13 16:00:05 +0100

    finished cleanup

--- Code/preprocessing/Emmisionscalculater.py
+++ Code/preprocessing/Emmisionscalculater.py
@@ -25,6 +25,7 @@ class Flight:
         """
         #TODO: calculate mass based on id
         self.CO2, self.H2O, self.NOx, self.HC, self.CO = [None] * 5
+        self.CO2rate, self.H2Orate, self.NOxrate, self.HCrate, self.COrate = [None] * 5
         self.ID = EURCTRLID
         self.type = AircraftDictionary_Eurocontrol_and_Aircraft[EURCTRLID]
         self.mass = mass
@@ -38,26 +39,31 @@ class Flight:
 
         ############### this is the order of steps###############################
         self.time_diffs = self.calcTimeDiffs()
-        self.DistHor = self.calcDistHorizontal()
+        self.time_cum = np.cumsum(self.time_diffs)
+        self.DistHor = self.calcDistHorizontal() #distance steps
         self.alts = np.array(self.flightData['Flight Level'])*100
         self.DistVert = self.calcDistVertical()
 
-        self.spdHor = self.calcDistHorizontal()
+        self.spdHor = self.calcSpdHorizontal()
         self.spdVert = self.calcSpdVertical()
 
         self.FF = self.initializeFF()
 
-        print(self.FF, self.alts, self.spdHor)
-        print(len(self.FF), len(self.alts), len(self.spdHor))
-        quit()
 
-        rates = [func() for func in [self.calcCO2Rate, self.calcH2ORate, self.calcNOxRate, self.calcCORate, self.calcHCRate]]
-        print("\n",rates)
+        #--------------------------------------------------------------------------------------------------------------------------------------
+        self.CO2rate = self.calcCO2Rate() * 1e-6
+        self.H2Orate = self.calcH2ORate() * 1e-6
+        self.NOxrate = self.calcNOxRate() * 1e-6
+        self.COrate = self.calcCORate() * 1e-6
+        self.HCrate = self.calcHCRate() * 1e-6
+
+        #calculates the total emission in tons
+        self.CO2 = self.integrateRate(rate=self.CO2rate) 
+        self.NOx = self.integrateRate(rate=self.NOxrate) 
+        self.H2O = self.integrateRate(rate=self.H2Orate) 
+        self.CO = self.integrateRate(rate=self.COrate) 
+        self.HC = self.integrateRate(rate=self.HCrate) 
         
-
-        #for var, rate in zip([self.CO2          , self.H2O          , self.NOx          , self.CO          , self.HC], 
-        #                     [self.calcCO2Rate(), self.calcH2ORate(), self.calcNOxRate(), self.calcCORate(), self.calcHCRate()]):
-        #    var = self.integrateRate(rate=rate)
         
         ############################################################################
 
@@ -108,7 +114,7 @@ class Flight:
 
         for now we assume that this groundspeed is close enough to the TAS
         """
-        spdHor = self.DistHor / self.time_diffs() * 1.943844
+        spdHor = self.DistHor / self.time_diffs * 1.943844
 
         return spdHor
     
@@ -173,227 +179,102 @@ class Flight:
         for i in range(1, len(rate)):  # Start from 1 since first position is 0
             arr[i] = arr[i-1] + 0.5 * (rate[i-1] + rate[i]) * self.time_diffs[i-1]
         return arr
+    
+    def plotFlightData(self, x=[]):
+        """
+        plots the flightpoint data after minimal processing
+        """
+        if len(x)==0:
+            x=self.time_cum
 
+        plt.subplot(321)
+        plt.scatter(x, self.alts[1:], marker=".", color='green', lw=0)
+        plt.ylabel("altitude (ft)")
 
+        plt.subplot(322)
+        plt.scatter(x, np.cumsum(self.DistHor), marker=".",color='pink', lw=0)
+        plt.ylabel("dist horizontal(steps) m")
 
+        plt.subplot(323)
+        plt.scatter(x, self.spdHor, marker=".",color='blue', lw=0)
+        plt.ylabel("speed (knots)")
+
+        plt.subplot(324)
+        plt.scatter(x, self.spdVert, marker=".",color='red', lw=0)
+        plt.ylabel("roc (fpm)")
+
+        plt.subplot(325)
+        plt.scatter(x, self.FF, marker=".",color='black', lw=0)
+        plt.ylabel("FuelFlow (kg/s)")
+
+        plt.show()
+        return
+    
+    def plotEmissionData(self, x=[], tot=True):
+        """
+        plots the emission in function of the input
+        if tot is true => plots the cumulative data
+        if tot=False => plots the rates
+        """
+        if len(x)==0:
+            x=self.time_cum
+        if tot==True:
+            vals = [self.CO2, self.H2O, self.NOx, self.HC, self.CO]
+        else:
+            vals = [self.CO2rate, self.H2Orate, self.NOxrate, self.HCrate, self.COrate]
+
+        plt.subplot(321)
+        plt.scatter(x, vals[0], marker=".", color='green', lw=0)
+        plt.ylabel("CO2")
+
+        plt.subplot(322)
+        plt.scatter(x, vals[1], marker=".",color='pink', lw=0)
+        plt.ylabel("H20")
+
+        plt.subplot(323)
+        plt.scatter(x, vals[2], marker=".",color='blue', lw=0)
+        plt.ylabel("Nox")
+
+        plt.subplot(324)
+        plt.scatter(x, vals[3], marker=".",color='red', lw=0)
+        plt.ylabel("CO")
+
+        plt.subplot(325)
+        plt.scatter(x, vals[4], marker=".",color='black', lw=0)
+        plt.ylabel("Hydrocarbons")
+
+        plt.show()
+        return
+
+    def plotGlobe(self):
+        """
+        plots a globe of the flight
+        """
+        lat_rad = np.radians(self.flightData['Latitude'])
+        lon_rad = np.radians(self.flightData['Longitude'])
+
+        radius = 1
+        x = radius * np.cos(lat_rad) * np.cos(lon_rad)
+        y = radius * np.cos(lat_rad) * np.sin(lon_rad)
+        z = radius * np.sin(lat_rad)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, c='b', marker='o')
+
+        u, v = np.mgrid[0:2 * np.pi:100j, 0:np.pi:50j]
+        xs = np.cos(u) * np.sin(v)
+        ys = np.sin(u) * np.sin(v)
+        zs = np.cos(v)
+
+        ax.plot_wireframe(xs, ys, zs, color='lightgray', alpha=0.5)
+        ax.set_xlabel('X Axis')
+        ax.set_ylabel('Y Axis')
+        ax.set_zlabel('Z Axis')
+        ax.set_box_aspect([1,1,1])  # Equal aspect ratio
+        plt.show()
 ############################################################################################################################################################
 
 test = Flight(238925251, 340000)
-
-print(test.CO2)
-#EuroControlID=238927688
-#PlaneType=AircraftDictionary_Eurocontrol_and_Aircraft[EuroControlID] #defines the planetype that will be used 
-#print(PlaneType)   
-#m_A320=340000     #in kg 
-#fuelflow = FuelFlow(ac=PlaneType)  #imports the specific flow for the type of aircraft
-#emission = Emission(ac=PlaneType)  #imports the specific emmisions for the type of aircraft
-#
-#
-#df=extract_ECTRLIDSeq('Data/PositionData/March')[EuroControlID]  #imports the data
-#seq=df["Sequence Number"]
-#Time=df["Time Over"]
-#Lattitude=df["Latitude"]
-#Longitude=df["Longitude"]
-#FlightLevel=df["Flight Level"]
-#hours=[]
-#minutes=[]
-#Lat=[]
-#Long=[]
-#FL=[]
-#
-#
-#for row in Time: #imports the time as a list to make a list of hours and minutes 
-#    hours.append(int(row[-5:-3]))
-#    minutes.append(int(row[-2:]))
-#Lat.extend(Lattitude.astype(float).tolist())  #makes a float list of lattitude 
-#Long.extend(Longitude.astype(float).tolist()), #makes a float list of longtude 
-#FL.extend(FlightLevel.astype(float).tolist()), #makes a float list of the flight level  
-#
-#delta=0    
-#TimeStamp=[]
-#Ttot=0
-#def DeltaTime(h1,m1,h2,m2):   #makes a function that calculates the time difference between two time points (unit minuts)
-#    delta=0
-#    if m2>m1:
-#        delta=m2-m1
-#    if m2<m1:
-#        delta=60-m1+m2
-#    if m2==m1:
-#        delta=0.5
-#    return delta
-#
-#Delta=[]
-#for i in range(len(minutes)-2):    #makes a list of delta values.
-#    Delta.append(DeltaTime(hours[i],minutes[i],hours[i+1],minutes[i+1]))
-#
-#def DistanceCalc(lat1,long1,lat2,long2): #returns the distance betweem two points in km 
-#    p1=(lat1,long1)
-#    p2=(lat2,long2)
-#    D=haversine(p1,p2)
-#    return D
-#
-#def VerSpeedCalc(dt,FL1,FL2):
-#    v_vertical=(FL2-FL1)/dt*100   #vertical flight speed in ft/min
-#    return v_vertical
-#def HorSpeedCalc(dt,D):      #horizontal flight speed in kts
-#    v_horizontal=(D)/dt/60*1000*1.943844
-#    return v_horizontal
-#Distances=[]
-#VerSpeeds=[]
-#HorSpeeds=[]
-#Dis=0
-#DisTot=0
-#DeltaTot=0
-#Ttot=[]
-#FLTot=[]
-#FuelFlow1=[]
-#TotalFuel=0
-#CO2Emmission=[]
-#H2OEmmission=[]
-#NOxEmmission=[]
-#COEmmission=[]
-#TotalCO2Emmission=0
-#TotalH2OEmmission=0
-#TotalNOxEmmission=0
-#TotalCOEmmission=0
-#
-#
-#for i in range(len(seq)-2):
-#    DisTot=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])+DisTot   #Caluculates the total distance travelled in km 
-#    Dis=DistanceCalc(Lat[i],Long[i],Lat[i+1],Long[i+1])   #Calculate the distance between two sequence point in km 
-#    Distances.append(DisTot)  # Makes a list of the total distances at a specific point in a journey 
-#    DeltaTot=Delta[i]+DeltaTot  # creates 
-#    FLTot.append(FL[i])
-#    Ttot.append(DeltaTot)
-#    VerticalSpeed=VerSpeedCalc(Delta[i],FL[i],FL[i+1])
-#    VerSpeeds.append(VerticalSpeed)
-#    HorizontalSpeed=HorSpeedCalc(Delta[i],Dis)
-#    HorSpeeds.append(HorizontalSpeed)
-#    FF = fuelflow.enroute(mass=m_A320, tas=HorSpeeds[i], alt=FL[i]*100, vs=VerSpeeds[i])
-#    TotalFuel=TotalFuel+FF*Delta[i]*60
-#    FuelFlow1.append(FF)
-#    CO2Emmission.append(emission.co2(FF))  # g/s
-#    H2OEmmission.append(emission.h2o(FF))  # g/s
-#    NOxEmmission.append(emission.nox(FF, tas=HorSpeeds[i], alt=FL[i]*100))  # g/s
-#    COEmmission.append(emission.co(FF, tas=HorSpeeds[i], alt=FL[i]*100))  # g/s
-#
-#
-#
-#for i in range(len(CO2Emmission)-1):
-#    TotalCO2Emmission=TotalCO2Emmission+abs(Delta[i]*(CO2Emmission[i]))*60 # g
-#    TotalH2OEmmission=TotalH2OEmmission+abs(Delta[i]*(H2OEmmission[i+1]+H2OEmmission[i])/2)*60# g
-#    TotalNOxEmmission=TotalNOxEmmission+abs(Delta[i]*(NOxEmmission[i+1]+NOxEmmission[i])/2)*60# g
-#    TotalCOEmmission=TotalCOEmmission+abs(Delta[i]*(COEmmission[i+1]+COEmmission[i])/2) *60# g
-#
-#print("Total CO2 emmisions [kg]=", TotalCO2Emmission/1000)
-#print("Total H2O emmisions [kg]=", TotalH2OEmmission/1000)
-#print("Total HOx emmisions [kg]=", TotalNOxEmmission/1000)
-#print("Total CO emmisions [kg]=", TotalCOEmmission/1000)
-#print("Total fuel spent=",TotalFuel)
-#
-#
-#plt.subplot(221)
-#plt.scatter(Ttot, FLTot, marker=".", color='green', lw=0)
-#plt.ylabel("altitude (ft)")
-#
-#plt.subplot(222)
-#plt.scatter(Ttot, HorSpeeds, marker=".",color='blue', lw=0)
-#plt.ylabel("speed (m/s)")
-#
-#plt.subplot(223)
-#plt.scatter(Ttot, VerSpeeds, marker=".",color='red', lw=0)
-#plt.ylabel("roc (fpm)")
-#
-#plt.subplot(224)
-#plt.scatter(Ttot, FuelFlow1, marker=".",color='black', lw=0)
-#plt.ylabel("FuelFlow (kg/s)")
-#
-#plt.show()
-#
-#
-#plt.subplot(221)
-#plt.plot(Ttot, CO2Emmission,marker=".", color='green',linestyle='-', lw=2)
-#plt.ylabel("CO2Emmission (g/s)")
-#
-#plt.subplot(222)
-#plt.plot(Ttot, H2OEmmission, marker=".",color='blue',linestyle='-', lw=2)
-#plt.ylabel("H2OEmmission (g/s)")
-#
-#plt.subplot(223)
-#plt.plot(Ttot, NOxEmmission, marker=".",color='red',linestyle='-', lw=2)
-#plt.ylabel("NOxEmmission (g/s)")
-#
-#plt.subplot(224)
-#plt.plot(Ttot, COEmmission, marker=".",color='black',linestyle='-', lw=2)
-#plt.ylabel("COEmmission (g/s)")
-#
-#
-#plt.show()
-#
-#
-#fig, axes = plt.subplots(2, 4, figsize=(15, 8))
-#
-## First row
-#axes[0, 0].scatter(Ttot, FLTot, marker=".", color='green', lw=0)
-#axes[0, 0].set_ylabel("Altitude (ft)")
-#
-#axes[0, 1].scatter(Ttot, HorSpeeds, marker=".", color='blue', lw=0)
-#axes[0, 1].set_ylabel("Speed (m/s)")
-#
-#axes[0, 2].scatter(Ttot, VerSpeeds, marker=".", color='red', lw=0)
-#axes[0, 2].set_ylabel("ROC (fpm)")
-#
-#axes[0, 3].scatter(Ttot, FuelFlow1, marker=".", color='black', lw=0)
-#axes[0, 3].set_ylabel("Fuel Flow (kg/s)")
-#
-## Second row
-#axes[1, 0].plot(Ttot, CO2Emmission, marker=".", color='green', linestyle='-', lw=2)
-#axes[1, 0].set_ylabel("CO2 Emission (g/s)")
-#
-#axes[1, 1].plot(Ttot, H2OEmmission, marker=".", color='blue', linestyle='-', lw=2)
-#axes[1, 1].set_ylabel("H2O Emission (g/s)")
-#
-#axes[1, 2].plot(Ttot, NOxEmmission, marker=".", color='red', linestyle='-', lw=2)
-#axes[1, 2].set_ylabel("NOx Emission (g/s)")
-#
-#axes[1, 3].plot(Ttot, COEmmission, marker=".", color='black', linestyle='-', lw=2)
-#axes[1, 3].set_ylabel("CO Emission (g/s)")
-#
-## Adjust layout
-#plt.tight_layout()
-#plt.show()
-#
-#
-## Convert latitude and longitude to radians
-##lat_rad = np.radians(Lat)
-##lon_rad = np.radians(Long)
-#
-## Assume a unit sphere (radius = 1)
-##radius = 1
-#
-## Convert to Cartesian coordinates
-##x = radius * np.cos(lat_rad) * np.cos(lon_rad)
-##y = radius * np.cos(lat_rad) * np.sin(lon_rad)
-##z = radius * np.sin(lat_rad)
-#
-## Create a 3D plot
-##fig = plt.figure()
-##ax = fig.add_subplot(111, projection='3d')
-#
-## Plot points on the sphere
-##ax.scatter(x, y, z, c='b', marker='o')
-#
-## Draw the sphere for reference
-##u, v = np.mgrid[0:2 * np.pi:100j, 0:np.pi:50j]
-##xs = np.cos(u) * np.sin(v)
-##ys = np.sin(u) * np.sin(v)
-##zs = np.cos(v)
-##ax.plot_wireframe(xs, ys, zs, color='lightgray', alpha=0.5)
-#
-## Set labels and aspect
-##ax.set_xlabel('X Axis')
-##ax.set_ylabel('Y Axis')
-##ax.set_zlabel('Z Axis')
-##ax.set_box_aspect([1,1,1])  # Equal aspect ratio
-#
-##plt.show()
+test.plotEmissionData(np.cumsum(test.DistHor), tot=False)
+test.plotGlobe()
