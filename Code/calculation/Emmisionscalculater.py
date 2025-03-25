ccb50ab111127150d660b4a9a67f8900860c6922
Michael Gerits <michael.gerits@alchemich.be>
2025-03-25 11:04:13 +0100

    tried to fix time read

--- Code/calculation/Emmisionscalculater.py
+++ Code/calculation/Emmisionscalculater.py
@@ -17,23 +17,6 @@ from preprocessing.AircraftIDandType import aircraft_dict
 from preprocessing.AircraftIDandType import aircraft_dict_mass
 
 
-
-#Load the data for al the required flights once
-Data = extract_ECTRLIDSeq('Data/PositionData/March')
-
-print("--------------------removing invalid aircraft types---------------------")
-print(f"old dataset length: {len(Data)}")
-# gets a list with eurocontrol id's with that invalid type
-invalid_ID = [id for id, type in AircraftDictionary_Eurocontrol_and_Aircraft.items() if type not in list(aircraft_dict.keys())]
-#delete flights with that type from data. to increase speed
-for ID in invalid_ID:
-    if ID in Data["keys"]:
-        Data["keys"].remove(ID)
-        Data.pop(ID, None)
-print(f"new dataset length: {len(Data)}")
-print("--------------------done---------------------")
-
-
 #############################################################################################################################################################
 #Make a class for a flight
 class Flight:
@@ -67,7 +50,7 @@ class Flight:
         # Fuel Flow & Emission objects are NOT initialized here to avoid pickling issues
 
     def initialize_emission(self):
-        """Does the openAP calculations afetr setting up base parameters."""
+        """Does the openAP calculations after setting up base parameters."""
         try:
             self.fuelFlow = FuelFlow(ac=self.type)
             self.emission = Emission(ac=self.type)
@@ -97,7 +80,11 @@ class Flight:
 
         returns the array AND updates the class variable
         """
-        time_diffs = np.array(pd.to_datetime(self.flightData['Time Over'], format='mixed').diff().dt.total_seconds().dropna())
+        try:
+            time_diffs = np.array(pd.to_datetime(self.flightData['Time Over'], format="%d/%m/%Y %H:%M").diff().dt.total_seconds().dropna())
+        except:
+            time_diffs = np.array(pd.to_datetime(self.flightData['Time Over'], format="%d/%m/%Y %H:%M:%S").diff().dt.total_seconds().dropna())
+
         #format="%d/%m/%Y %H:%M"
         return time_diffs
     
@@ -204,6 +191,7 @@ class Flight:
         a wrapper for integrating the rates, different methods of integrations can be added here
         the integration will be done in tons
         """
+        rate = np.abs(rate)
         arr = np.zeros(len(rate))  # Same size as velocity
 
         # Compute trapezoidal integration step by step
@@ -366,10 +354,10 @@ if __name__ == "__main__":
     #---------------------------------------------------------------------------------------------------------------------------------------------------------------
     print(f"--------------------initializing {len(Data)-2} flights ---------------------")
 
-    flights = np.array([])
-    for ID in tqdm(Data["keys"][1:], desc="Initializing objects", unit="flight"):
+    flights = []
+    for ID in tqdm(Data["keys"][:5], desc="Initializing objects", unit="flight"):
         obj = create_flight(ID, Data)
-        np.append(flights,obj)
+        flights.append(obj)
     
 
     # Filter out None values
@@ -377,7 +365,8 @@ if __name__ == "__main__":
 
     print("--------------------done---------------------")
     #--------------------------------------------------------------------------------------------
+    #print(flights)
 
-
-    print(flights)
+    flights[0].plotEmissionData(tot=False)
+    #print(flights[0].time_cum, flights[0].time_diffs)
 
