8797f7a2ae8a8808dc83976366ecb429183058dd
Michael Gerits <michael.gerits@alchemich.be>
2025-04-24 15:55:09 +0200

    findairports kinda works?

--- Code/preprocessing/AirportClassifier.py
+++ Code/preprocessing/AirportClassifier.py
@@ -2,8 +2,8 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv("Data\Airports\AirportsLongLat.csv")
-LongitudeAirport=np.ceil(df["lon"])
-LatAirport=np.floor(df["lat"])
+LatAirport=df["lat"]
+LongitudeAirport=df["lon"]
 Name_of_Airport=df["city"]
 LongitudeAirportlist=[]
 Name_of_Airportlist=[]
@@ -12,8 +12,13 @@ ID=[]
 
 Aiport_Classifier = {}
 for i in range(len(LongitudeAirport)):
-    key = LongitudeAirport[i]
-    key1=LatAirport[i]
+    key=np.ceil(LatAirport[i] * 1e1)
+    key1 = np.floor(LongitudeAirport[i] * 1e1)
     value = Name_of_Airport[i]
     Aiport_Classifier.update({(key,key1): value})
+
+#print(Aiport_Classifier[(501,85)])
+#50.03333,8.57056 => Frankfurt
+
+#51.5183,7.61224 => dortmund
 
